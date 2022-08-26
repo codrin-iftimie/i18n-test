0eea799c64facb362ca68a51f813d60f46dd4888
Codrin Iftimie <iceboxic@gmail.com>
2022-08-26 12:06:46 +0300

    Middleware fail

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: process.env.NODE_ENV === "production" ? ["en", "de", "fr", "nl"] : ["en", "de", "fr", "nl", "pseudo"],
+    defaultLocale: "en",
+  },
 }
 
 module.exports = nextConfig
---
+++ src/middleware.js
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+export async function middleware(request) {
+  let response = NextResponse.next();
+
+  if (request?.nextUrl?.locale && request.nextUrl.locale === "de") {
+    if (request.nextUrl.pathname === "/test-de") {
+      return NextResponse.rewrite(new URL("/test", request.url));
+    }
+  }
+
+  return response;
+}
